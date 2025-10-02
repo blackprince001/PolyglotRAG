2800f971b4edc8c02361c050e8e8b1e2cd1ba551
blackprince001 <appiahboaduprince@gmail.com>
2025-10-02 14:51:55 +0000

    all routes for http service

---
+++ src/presentation/http/routes/mod.rs
@@ -0,0 +1,9 @@
+pub mod file_routes;
+pub mod search_routes;
+pub mod health_routes;
+pub mod job_routes;
+
+pub use file_routes::*;
+pub use search_routes::*;
+pub use health_routes::*;
+pub use job_routes::*;
